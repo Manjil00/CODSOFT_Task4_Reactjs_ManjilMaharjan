8ab3bd28a5f658334e8b3ca8909e3bdecfbfa896
ACER <210220@softwarica.edu.np>
2024-10-16 18:40:27 +0545

    incrdecr cart

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -6,8 +6,30 @@ return (
     <div className="main w-full h-[100vh] bg-appleblack">
     <Nav/>
     
-    <div className="Details">
+    <div className="details bg-appleblack h-[87vh] w-full p-2 md:p-5 mt-5">
+        <div className="productdetails flex justify-evenly items-start">
+
+            <img src='../images/testpic.jpg' className='h-[250px] w-[40%] md:h-[500px] md:w-[40%] rounded-xl' alt='img'/>
         
+        <div className="description h-auto w-[40%] flex flex-col justify-start items-start gap-5 ">
+                <h1 className='font-sans font-bold text-start text-2xl md:text-4xl text-white'>Insta 360 X3 Camera</h1>
+                <p className='font-sans text-start text-sm md:text-xl text-white'>Lorem, ipsum dolor sit amet consectetur
+                    adipisicing elit. Nobis saepe ipsum, eius exercitationem rem nulla?
+                    Delectus explicabo nisi aspernatur, dolorem aliquam fuga praesentium corrup,kj,jhhjjh,h,hj,hj,jh,jh,jh,jh,jh,jhti laboriosamdsfsfdsfdsfdsfds
+                    animi minima asperdsfsfsfsfsfsfsfsfsfsfsfsfsfsfsfsfsfsfsfsfsfsfsfsfsfsfsfsfsfsfsfsfsfsfsfiores ratione laborum?</p>
+
+                <div className="incrDecr mt-5 bg-green-800 h-[50px] w-[200px] rounded-xl flex justify-between items-center">
+                    <button className='h-[50px] w-[50px] rounded-tl-xl rounded-bl-xl  text-white'>-</button>
+                    <h1 className='font-bold font-sans text-center'>0</h1>
+                    <button className='h-[50px] w-[50px] rounded-tr-xl rounded-br-xl text-white '>+</button>
+
+                </div>
+
+                <button
+                className='h-[50px] w-[150px] bg-green-600 rounded-xl mt-5'>Add To Cart</button>
+        </div>
+
+        </div>
     </div>
 
     </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -13,7 +13,7 @@ return (
 cursor-pointer flex justify-center items-center'>Product Catalog</h1>
 
     </div>
-    <div className={`absolute min-h-[30vh] left-0 top-[90%] w-full p-5 text-white flex flex-col md:static md:flex md:min-h-fit md:w-auto gap-9 text-sm ${isNavOpen ? 'block' : 'hidden'}`}>
+    <div className={`absolute min-h-[30vh] left-0 top-[90%] w-full p-5 bg-appleblack text-white flex flex-col md:static md:flex md:min-h-fit md:w-auto gap-9 text-sm ${isNavOpen ? 'block' : 'hidden'}`}>
         <ul className=" cursor-pointer flex flex-col gap-4 md:flex-row  md:gap-8 md:justify-evenly md:text-xl font-sans">
         <li className="hover:text-slate-600">Home</li>
         <li className="hover:text-slate-600">Products</li>
