a99e0d1170d461ff4dd85e5ca56f6d90ac892fba
ACER <210220@softwarica.edu.np>
2024-10-16 18:13:54 +0545

    components

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import Nav from './Nav';
 
 const Details = () => {
 return (
-    <div className="main w-full h-[100vh]">
+    <div className="main w-full h-[100vh] bg-appleblack">
     <Nav/>
-
+    
     <div className="Details">
-
+        
     </div>
 
     </div>
