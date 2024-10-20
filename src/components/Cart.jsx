7e195a30f09fed34e9168b5c152a24cb7b6ec915
ACER <210220@softwarica.edu.np>
2024-10-20 22:05:10 +0545

    cmnt

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,30 +1,35 @@
-// Cart.jsx
 import React from 'react';
-import { useSelector } from 'react-redux';
-import Nav from './Nav';
+import Nav from "./Nav";
+//ICONS
+import { RiDeleteBin5Line } from "react-icons/ri";
+
+import { useSelector } from "react-redux";
 
 const Cart = () => {
-    const cartItems = useSelector((state) => state.cart.cartItems); // Access cart items from the Redux store
+    const cartItems=useSelector((state)=>state.cart.cartItems);
 
-    return (
-        <div className="cart-container">
-            <Nav />
-            <h1>Your Cart</h1>
-            {cartItems.length === 0 ? (
-                <p>Your cart is empty</p>
-            ) : (
-                <div>
-                    {cartItems.map((item) => (
-                        <div key={item.id} className="cart-item">
-                            <img src={item.image} alt={item.title} />
-                            <h2>{item.title}</h2>
-                            <p>${item.price}</p>
-                        </div>
-                    ))}
-                </div>
-            )}
+return (
+    <div className='main w-full h-[800px] bg-appleblack'>
+    <Nav/>
+
+    <div className="cartSection p-5">
+    {
+    cartItems.length === 0 ?
+    <h1>Empty Cart</h1> :
+    cartItems.map((item) => (
+        <div key={item.id} className="product1 h-[150px] w-full bg-slate-300 flex justify-evenly items-center rounded-xl">
+            <img src={item.image} className='h-[150px] w-[150px]' alt='productimg'/>
+            <h1 className='text-black font-semibold font-sans text-xl md:text-2xl'>{item.title}</h1>
+            <p className='text-black font-sans text-sm md:text-xl'>Quantity: 10</p>
+            <button className='h-[100px] w-[100px]'><RiDeleteBin5Line /></button>
         </div>
-    );
-};
+    ))
+}
+    
+    </div>
+
+    </div>
+)
+}
 
 export default Cart;
--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -1,16 +1,16 @@
-const initialstate={
-    cartItems:[],
-};
+// const initialstate={
+//     cartItems:[],
+// };
 
-const cartReducer =(state=initialstate,action)=>{
-    switch (action.type){
-        case ADD_TO_CART : 
-        return{
-            ...state,
-            cartItems: [...state.cartItems,action.payload]
-        };
-        default: return state;
-    };
-};
+// const cartReducer =(state=initialstate,action)=>{
+//     switch (action.type){
+//         case ADD_TO_CART :
+//         return{
+//             ...state,
+//             cartItems: [...state.cartItems,action.payload]
+//         };
+//         default: return state;
+//     };
+// };
 
-export default cartReducer;
+// export default cartReducer;
